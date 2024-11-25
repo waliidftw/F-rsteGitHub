1fbb18e0662c0e743d61a97e1a865b1fb9f72215
waliidftw <wtaa67743@edu.ucl.dk>
2024-11-25 11:27:50 +0100

    created new project

---
+++ FørsteGitHub/FørsteGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FørsteGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
